05517537e35fc4c69b3b93a8bb27de5ae2e9770f
Lydia Gao <l75gao@uwaterloo.ca>
2025-04-30 00:09:28 -0400

    009

--- books.py
+++ books.py
@@ -6,7 +6,7 @@ BOOKS = [
     {'title': 'Title one', 'author': 'Author one', 'category': 'science'},
     {'title': 'Title two', 'author': 'Author two', 'category': 'history'},
     {'title': 'Title three', 'author': 'Author three', 'category': 'science'},
-    {'title': 'Title four', 'author': 'Author two', 'category': 'history'},
+    {'title': 'Title four', 'author': 'Author two', 'category': 'science'},
     {'title': 'Title five', 'author': 'Author four', 'category': 'math'},
     {'title': 'Title six', 'author': 'Author three', 'category': 'history'}
 ]
@@ -22,3 +22,23 @@ async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(boook_author: str, category: str):
+    books_to_rerurn = []
+    for book in BOOKS:
+        if book.get('author').casefold() == boook_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_rerurn.append(book)
+    
+    return books_to_rerurn
