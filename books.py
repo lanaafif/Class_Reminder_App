56b00b000372845997628b14953c126543f8f755
Lydia Gao <l75gao@uwaterloo.ca>
2025-04-29 23:41:22 -0400

    007

--- books.py
+++ books.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def first_api():
-    return {"message": "Hello Lydia"} 
+BOOKS = [
+    {'title': 'Title one', 'author': 'Author one', 'category': 'science'},
+    {'title': 'Title two', 'author': 'Author two', 'category': 'history'},
+    {'title': 'Title three', 'author': 'Author three', 'category': 'science'},
+    {'title': 'Title four', 'author': 'Author two', 'category': 'history'},
+    {'title': 'Title five', 'author': 'Author four', 'category': 'math'},
+    {'title': 'Title six', 'author': 'Author three', 'category': 'history'}
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
