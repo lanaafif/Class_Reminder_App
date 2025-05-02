1d45d4ff37ede144ca272539f4350b639bd82d6b
Lydia Gao <l75gao@uwaterloo.ca>
2025-05-02 19:01:02 -0400

    001

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
