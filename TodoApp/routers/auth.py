a5d22eeab964d7950c0384c303a42935a40f607b
Lydia Gao <l75gao@uwaterloo.ca>
2025-05-04 23:42:32 -0400

    p65

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime, timezone
 from typing import Annotated
 from database import SessionLocal
 from fastapi import APIRouter, Depends, HTTPException, Path, status
@@ -5,12 +6,20 @@ from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
+
+SECRET_KEY = 'c376b1e1b6112627129877f30793f8b00598adb28659f1f5f8fb6614ce38540e'
+ALGORITHM = 'HS256'  # algorithm used to encode the JWT token 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto") 
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")  # this is the url to get the token
 
 
 class CreateUserRequest(BaseModel):
@@ -20,6 +29,11 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str 
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 def get_db():
@@ -40,10 +54,32 @@ def authenticate_user(db, username: str, password: str):
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
-    return True
+    return user
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub':username, 'id':user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):      
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: str = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                detail="Could not validate user.",)
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                            detail="Could not validate user.",)
+                                                                        
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         user_name=create_user_request.username,
@@ -59,10 +95,12 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.commit()
 
 
-@router.post("/token", status_code=status.HTTP_200_OK)
+@router.post("/token", status_code=status.HTTP_200_OK, response_model=Token)
 async def login_for_access_token(db: db_dependency, 
                                  form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
-        return 'Failed Authentication'
-    return 'Successful Authentication'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                            detail="Could not validate user.",)
+    token = create_access_token(user.user_name, user.id, timedelta(minutes=20))
+    return {'access_token': token, 'token_type': 'bearer'}
--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -4,6 +4,7 @@ from models import Todos
 from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from .auth import get_current_user
 
 
 router = APIRouter()
@@ -19,6 +20,8 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
 
 
 class TodoRequest(BaseModel):
