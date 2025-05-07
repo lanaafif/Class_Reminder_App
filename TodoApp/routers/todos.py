94164f0981b7a29bb7b485ca8c6cced1402d0e56
Lydia Gao <l75gao@uwaterloo.ca>
2025-05-07 19:12:18 -0400

    navbar

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -59,7 +59,7 @@ async def render_todo_page(request: Request, db: db_dependency):
         
         todos = db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
 
-        return templates.TemplateResponse("todo.html", {"request": request, "todos": todos})
+        return templates.TemplateResponse("todo.html", {"request": request, "todos": todos, "user": user})
     
     except:
         return redirect_to_login()
