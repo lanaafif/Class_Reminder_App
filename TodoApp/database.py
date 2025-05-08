f9aded7050075708c41498c4ce367ff2cc3e6319
Lydia Gao <l75gao@uwaterloo.ca>
2025-05-08 00:27:08 -0400

    railway deploy

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,18 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:060204@localhost/TodoApplicationDatabase'
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  # SQLite database URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    DATABASE_URL = 'postgresql://postgres:060204@localhost/TodoApplicationDatabase'
+
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
-
-
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  # SQLite database URL
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
